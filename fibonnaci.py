8e8a6c8f009625ebb32c2d283747cc566dde1fac
Htunn Thu Thu <htunnthuthu@stellaris.holdings>
2022-03-07 09:57:48 +0630

    adding fibonnaci python script

---
+++ fibonnaci.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def getFibonnaciSeries(num):
+    c1, c2 = 0,	1
+    count = 0
+    while count < num:
+        yield c1
+        c3 = c1 + c2
+        c1 = c2
+        c2 = c3
+        count += 1
+
+
+fin = getFibonnaciSeries(7)
+print(fin)
+for i in fin:
+    print(i)
